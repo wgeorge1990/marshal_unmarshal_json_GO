10a5785710a9179d9e86a03e2deafcf9697f74e2
William George <github email address>
2019-09-30 17:37:23 -0400

    Refactored unMarshalJSON to independent function and corrected spelling.

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 )
 
-// Type art is not exportable to other packages because naming is lowercased. If export is needed, upercase custom type name.
-// Fields are uppercase which allow Marshal and Unmarshal accesss from the encoding/json pkg.
+// Type art is not exportable to other packages because naming is lowercased. If export is needed, uppercase custom type name.
+// Fields are uppercase which allow Marshal and Unmarshal access from the encoding/json pkg.
 
 // type artist struct {
 // 	Name string `json:"Name"`
@@ -40,18 +40,22 @@ func convertToJSON(v interface{}) []byte {
 	return bs
 }
 
-func unMarshalJSON(bs []byte, a artist{}) {
+func unMarshalJSON(bs []byte, a *artist) {
+	//use type pointer
 	err := json.Unmarshal(bs, a)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
 func main() {
-	//Generate sample data to Marshal into JSON.
+	// Generate sample data to Marshal into JSON.
 	artOne := art{Name: "paintingOne", Medium: "paint", Price: 1000}
 	artTwo := art{Name: "paintingTwo", Medium: "paint", Price: 1000}
 	artThree := art{Name: "paintingThree", Medium: "paint", Price: 1000}
 	artistOne := artist{Name: "will", Age: 29, Art: []art{artOne, artTwo, artThree}}
-	//call function and pass in artistOne variable which returns []bytes and is then converted into
-	// a string inside of the print function on line 56 : string([]byte)
+	// Call function and pass in artistOne variable which returns []bytes and is then converted into
+	// A string inside of the print function on line 56 : string([]byte)
 	jsonOne := convertToJSON(artistOne)
 	// fmt.Println(string(jsonOne))
 	//Print type []uint8 which is same as []byte
@@ -63,11 +67,16 @@ func main() {
 	//         OR
 	//______name________type__
 	 singleArtist := artist{}
-	err := json.Unmarshal(jsonOne, &singleArtist)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println("all of the data", singleArtist)
 
+	 unMarshalJSON(jsonOne, &singleArtist)
+	 fmt.Println(singleArtist)
+
+	 // Or do inline bellow -->
+
+	// err := json.Unmarshal(jsonOne, &singleArtist)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// fmt.Println("all of the data", singleArtist)
 
 }
